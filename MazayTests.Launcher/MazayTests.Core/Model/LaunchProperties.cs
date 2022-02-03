4f558beb87b04d01de9063a05132cf066c28fe3a
roma_outspin <roma_outspin@mail.ru>
2022-02-03 23:21:22 +0300

    LaunchProperties
    -public class
    -added CutomMode Class

--- MazayTests.Launcher/MazayTests.Core/Model/LaunchProperties.cs
+++ MazayTests.Launcher/MazayTests.Core/Model/LaunchProperties.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace MazayTests.Core.Model
 {
-    class LaunchProperties
+    public class LaunchProperties
     {
+        
+        public TimeSpan Timer { get; set; }
+        public bool withNextButton { get; set; }
+        public bool withBackButton { get; set; }
+        public int QuestionCount { get; set; }
+        public bool QuestionRandom { get; set; }
+        public bool AnswerRandom { get; set; }
+        //public List<Question> SelectedQuestions { get; set; }
+
+        private CustomMode _cutomMode;
+
     }
 }
